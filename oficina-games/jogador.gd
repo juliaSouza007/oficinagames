e0ff3338c096ff811898ae76101f60465e9cff69
Júlia Martins Souza <juliamss07@gmail.com>
2025-11-25 08:16:43 -0300

    Delete oficina-games directory

--- oficina-games/jogador.gd
+++
@@ -1,9 +0,0 @@
-class_name Jogador extends Area2D
-
-var velocidade:float = 500
-
-func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_RIGHT) or Input.is_key_pressed(KEY_D):
-		self.position.x += velocidade * delta;
-	if Input.is_key_pressed(KEY_LEFT) or Input.is_key_pressed(KEY_A):
-		self.position.x -= velocidade * delta;
